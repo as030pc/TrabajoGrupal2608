852d6bdabedbb12d8deb2a76d9aa560e956d4a0c
Andres Palma <palma.castaneda030@gmail.com>
2021-08-26 13:01:28 -0500

    Actualizacion2

--- scripts/class/InterfaceUsuario.js
+++ scripts/class/InterfaceUsuario.js
@@ -1,4 +1,26 @@
 export default class InterfaceUsuario{
+    
+    url = 'http://localhost:4000/usuarios/'
+
+    constructor(nombre, apellido, correo){
+        this.nombre = nombre,
+        this.apellido = apellido,
+        this.correo = correo
+    }
+    divMostrarPerfil(perfil) {
+        axios.get(perfil.url)
+        const {nombre, apellido, correo} = perfil;
+        let mostrarPerfil = document.getElementById('mostrar-perfil');
+        mostrarPerfil.innerHTML = ''
+        mostrarPerfil.innerHTML += `
+        <td>${nombre}</td>
+        <td>${apellido}</td>
+        <td>${correo}</td> `
+
+    }
+
+    
+
 
     
 }
--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,13 @@
 import Usuario from '../scripts/class/Usuario.js';
+import InterfaceUsuario from '../scripts/class/InterfaceUsuario.js';
 
 let formulario = document.getElementById('formulario');
 let btnCorreo = document.getElementById('btnCorreo');
 
+
+
 const usuario = new Usuario();
+const Interfaz = new InterfaceUsuario();
 
 formulario.addEventListener('submit', e => {
     e.preventDefault();
@@ -14,22 +18,36 @@ formulario.addEventListener('submit', e => {
     usuario.apellido = apellido;
     usuario.correo = correo;
     //console.log(usuario);
+    localStorage.setItem('user',JSON.Stringfy(usuario) )
+    usuarioNew =  JSON.parse(localStorage.getItem('user'))
     usuario.crearPerfil(usuario);
+    Interfaz.divMostrarPerfil(usuarioNew);
+    e.stopPropagation();
+    
 })
+document.addEventListener('DOMContentLoaded', ()=> {
+    if (localStorage.getItem('Buscado') ) {
+        usuarios = JSON.parse(localStorage.getItem('Buscado'));
+    }
+})
+
+
 
 btnCorreo.addEventListener('click', () => {
     let correos = document.getElementById('email').value;
     document.getElementById('email').readOnly = true;
     usuario.buscarPerfil(correos); 
-    let perfil = JSON.parse(localStorage.getItem('Buscado'));
+    let perfil = JSON.parse(localStorage.getItem('Buscado'));  //Existe un error en la manera con se
     console.log(perfil)
     const {nombre, apellido, correo,id} = perfil;
     document.getElementById('name').value = nombre;
     document.getElementById('lastName').value = apellido;
     document.getElementById('email').value = correo;
     document.getElementById('id').value = id;
-
+    Interfaz.divMostarPerfil(usuario);
 })
+
+
 
 btnEditar.addEventListener('click', () => {
     let id = document.getElementById('id').value;
